48cae57624b6003dd0e970338b42bc4843752976
Unknown <matias.fuentes.p@usach.cl>
2019-07-13 21:46:32 -0400

    f2

--- Flujo_experimental_2/flujo_experimental2.py
+++ Flujo_experimental_2/flujo_experimental2.py
@@ -126,7 +126,7 @@ for linea in archivo.readlines():
 	numero=numero+1
 escritor=0
 for matriz in listadenoticias:
-	if escritor ==1000:
+	if escritor ==3000:
 		print("estoyjuntando")
 		escritor=0
 	for a1 in matriz:
@@ -136,7 +136,7 @@ for matriz in listadenoticias:
 	escritor=escritor+1
 lista_nueva = []
 for indice in listadengrmas:
-    if escritor ==1000:
+    if escritor ==30000:
     	print("estoy filtrando")
     	escritor=0
     if indice not in lista_nueva:
@@ -147,7 +147,7 @@ encontrar=0
 ngramas=[]
 for palabra in lista_nueva:
 	for palabra2 in listadengrmas:
-		if escritor==3000:	
+		if escritor==80000:	
 			print("estoyalmacenando")
 			escritor=0
 		if palabra == palabra2:
@@ -167,7 +167,7 @@ for linea in topico.readlines():
 contador=0;
 cuentalinea=1
 for noticia in listadenoticias:
-	if escritor==1000:
+	if escritor==80000:
 		print("estoyescribiendo")
 		escritor=0
 	archivo6.write(str(cuentalinea))
